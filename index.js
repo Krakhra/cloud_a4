fffaa3e3713e3902804064f2d5e16b09e9eecdcd
Kirat <hrakhra@mail.uoguelph.ca>
2020-11-26 14:33:41 -0500

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const request = require('request')
+const bodyParser = require('body-parser')
+const AWS = require('aws-sdk')
+const Fs = require('fs')
+var url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=8c553c1ce53a433ca7434c77bf8ed27a';
 
 app.use('/', express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+app.use(bodyParser.json());
 
+app.get('/article',(req,res)=>{
+  request(url,function(error,response,body){
+    res.send(JSON.parse(body))
+  })  
+
+})
+
+app.post('/listen',(req,res)=>{
+  // create polly
+  const Polly = new AWS.Polly({
+    region: 'us-east-1'
+  })
+
+  let params = {
+    'Text': req.body.text,
+    'OutputFormat': 'mp3',
+    'VoiceId':'Kimberly'
+  }
+  Polly.synthesizeSpeech(params, (err, data) => {
+    if (err) {
+        console.log(err.code)
+    } else if (data) {
+        if (data.AudioStream instanceof Buffer) {
+            Fs.writeFile("./public/speech.mp3", data.AudioStream, function(err) {
+                if (err) {
+                    return console.log(err)
+                }
+                console.log("The file was saved!")
+            })
+        }
+    }
+  })  
+  console.log(req.body.text)
+  res.send('works')
+})
 app.listen(8000, ()=>{
   console.log('Server Running')
 })
--- public/query.js
+++ public/query.js
@@ -1,7 +1,27 @@
-function listen(){
-  alert('test listen')
+function random(max){
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-function getArticle(){
-  alert('get')
+function listen(event){
+  console.log(document.getElementById("exampleFormControlTextarea1").value)
+  axios({
+    method:'post',
+    url: '/listen',
+    data:{
+      text:document.getElementById("exampleFormControlTextarea1").value
+    }
+  })
+  .then(function(response){
+    console.log(response)
+  })
+}
+
+function getArticle(event){
+  axios.get('/article')
+    .then(function(response){
+      var target = event.target || event.srcElement;
+      randomNum = random(response.data.articles.length)
+      document.getElementById("exampleFormControlTextarea1").value = response.data.articles[randomNum].description
+      console.log(response.data.articles)
+    })
 }
